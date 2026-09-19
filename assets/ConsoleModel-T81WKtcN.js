9abbc29228799ab4c2ac1aa3607e60f3b40e76b5
Colcolat <jj.zapatabuenfil@gmail.com>
2026-09-19 15:14:14 -0600

    Updates

--- assets/ConsoleModel-T81WKtcN.js
+++ assets/ConsoleModel-T81WKtcN.js
@@ -1,4 +1,4 @@
-import{r as Si,j as oc,C as lc}from"./index-DlkPyvEw.js";/**
+import{r as Si,j as oc,C as lc}from"./index-DoARey6M.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
