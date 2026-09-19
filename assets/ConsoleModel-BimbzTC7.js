285de7b45867661bf54c6f2f39940e33a2cee2b6
Colcolat <jj.zapatabuenfil@gmail.com>
2026-09-19 03:07:10 -0600

    Updates

--- assets/ConsoleModel-BimbzTC7.js
+++ assets/ConsoleModel-BimbzTC7.js
@@ -1,4 +1,4 @@
-import{r as Si,j as oc,C as lc}from"./index-BuVZeA1s.js";/**
+import{r as Si,j as oc,C as lc}from"./index-BTpmJBiS.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
